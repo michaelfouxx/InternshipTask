692d2a0875a198337ec06baa9f36ec928974aaf1
michaelfouxx <230073841@aston.ac.uk>
2026-03-03 18:50:42 +0000

    Update script.js
    This JavaScript file controls the interactivity of the Sustainability Dashboard. It updates the CO₂ Savings counter and the progress bar based on the user’s data. It also enables the filter buttons, allowing users to switch between all food items, high-impact, and low-impact items. The code uses forEach loops, dataset attributes, and DOM manipulation to provide a responsive, interactive experience.

--- greencart-sustainability-dashboard/script.js
+++ greencart-sustainability-dashboard/script.js
@@ -1,19 +1,41 @@
-function filterItems(type) {
-  displayItems(type);
-  displayItems(high);
-  const cards = document.querySelectorAll('swap.card');
-  const buttons = document.querySelectorAll('filter.btwn');
-
-  buttons: forEach (btn = classList = remove('active'));
-}
+const filterButtons = document.querySelectorAll(".filter-btn");
+const foodItems = document.querySelectorAll(".food-item");
+const co2Display = document.getElementById("co2Value");
+const progressFill = document.getElementById("progressFill")
 
 
+  let co2Saved = 12.4;
+  let monthlyGoal = 20;
 
+  co2Display.textContent = co2Saved;
 
-  cards.forEach(card => {
-    if (card.classList.contains(type)) {
-      card.style.display = "block";
-    } else {
-      card.style.display = "none";
-    }
-  });
+  let progressPercentage = (co2Saved / monthlyGoal) * 100;
+  progressFill.style.width = progressPercentage + "%";
+
+  filterButtons.forEach(button => { 
+    
+    button.addEventListener("click", () => {
+
+      filterButtons.forEach(btn => btn.classList.remove("active"));
+
+      button.classList.add("active");
+
+      const filterValue = button.dataset.filter;
+
+      foodItems.forEach(item => {
+
+        if (filterValue === "all") {
+          item.style.display = "block";
+        }
+        else if (item.dataset.impact === filterValue){
+          item.style.display = "block";
+        }
+        else{
+          item.style.display = "none";
+        }
+        
+      });
+
+    });
+
+});
